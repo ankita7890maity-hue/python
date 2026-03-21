6d68aaaca1fb73bfbd4eca7d1531ebdd75b1238a
ankita7890maity-hue <ankita7890maity@gmail.com>
2026-03-21 17:23:52 +0530

    questionf 1 solved.

---
+++ decision_making/question1.py
@@ -0,0 +1,6 @@
+'''Take a integer number from user. 
+Print " Aur bhai, kya haal chaal " if the number is greater than 10.'''
+
+number = int(input("Enter a number : "))
+if number < 10:
+    print("Aur bhai, kya haal chaal?")
